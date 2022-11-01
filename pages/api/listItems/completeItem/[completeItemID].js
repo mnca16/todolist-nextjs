fc46506dcc8603030c90581185809639fa7d64f1
Monica Delgado <mnca.delgado@gmail.com>
2022-11-01 00:47:51 -1100

    made completed functionality with API

--- pages/api/listItems/completeItem/[completeItemID].js
+++ pages/api/listItems/completeItem/[completeItemID].js
@@ -4,18 +4,24 @@ import Items from "../../../../models/listItems"
 // NEXT.JS BUILT IN API DELETES LIST TITLES BY ID
 export default async function completedItem(req, res) {
   const { completeItemID } = req.query // ---> next.js dynamic API request helper
-  console.log("req.query", req.query)
+  console.log("completedItemID", completeItemID)
 
   try {
     await connectMongo()
-    await Items.findByIdAndUpdate(completeItemID, { completed: true })
-    const completedItem = await Items.findById(completeItemID)
-    console.log("completedItem", completedItem)
-    res.json({ completedItem })
+    const listItem = await Items.findById(completeItemID)
+    if (listItem.completed) {
+      await Items.findByIdAndUpdate(completeItemID, { completed: false })
+    } else {
+      await Items.findByIdAndUpdate(completeItemID, { completed: true })
+    }
+
+    const completedItemResponse = await Items.findById(completeItemID)
+    res.status(200).json({ completedItemResponse })
+
     if (!completedItem) {
       return res.json({ message: "there is not list available" })
     }
   } catch (error) {
-    res.json({ error })
+    res.status(500).json({ error })
   }
 }
