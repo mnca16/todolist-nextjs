aabcc5c0f2c570cf5c8138552455c0aa2261b8db
Monica Delgado <mnca.delgado@gmail.com>
2022-12-06 12:37:03 -1100

    installed react testing library

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
-  //testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jest-environment-jsdom",
   preset: "@shelf/jest-mongodb",
 }
 
