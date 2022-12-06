5aec290db92c9a41a253d848373722223ef0f689
Monica Delgado <mnca.delgado@gmail.com>
2022-12-06 12:21:06 -1100

    installed @shelf/jest-mongodb library and try to connect mongo

--- lib/connectMongo.ts
+++ lib/connectMongo.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose"
 
+
 const mongoUri: string = process.env.MONGO_URI || ""
 
 const connectMongo = async () => {
   if (mongoUri === "") {
     throw new Error('enviroment variable undefined')
   }
-  mongoose.connect(mongoUri).catch((err) => {console.log("DB connect error",err)})
+
+  await mongoose.connect(mongoUri).catch((err) => {console.log("DB connect error",err)})
 }
 
 export default connectMongo
